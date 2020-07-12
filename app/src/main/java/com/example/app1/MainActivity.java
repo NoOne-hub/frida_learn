bde8825530e258b8f64b251121a7674e43eb8ad9
NoOne <dotaerday@gmail.com>
2020-07-12 20:55:18 +0800

    update new

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,36 +1,38 @@
 package com.example.app1;
 
-import android.util.Log;
-
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private String total = "@@@###@@@";
+    EditText username_et;
+    EditText password_et;
+    TextView message_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        password_et = (EditText) this.findViewById(R.id.editText1);
+        username_et = (EditText) this.findViewById(R.id.editText1);
+        message_tv = (TextView) findViewById(R.id.textView);
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username_et.getText().toString().compareTo("admin") == 0) {
+                    message_tv.setText("You can't login as admin");
+                    return;
+                }
+                //hook taget
+                message_tv.setText("Sending to the server :" + Base64.encodeToString((username_et.getText().toString() + ":" + password_et.getText().toString()).getBytes(), Base64.DEFAULT));
             }
-            fun(50, 30);
-            Log.d("ROYSUE.string", fun("LoWeRcAsE Me!!!!!"));
-        }
+        });
     }
 
-    void fun(int x, int y) {
-        Log.d("Sum", String.valueOf(x + y));
-    }
-    String fun(String x){
-        total += x;
-        return x.toLowerCase();
-    }
-    String secret(){
-        return total;
-    }
+
 }
